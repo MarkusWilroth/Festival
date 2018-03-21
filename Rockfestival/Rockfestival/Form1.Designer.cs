f497a3a480f85ad9e6cfd3a35fb87b2c8243f277
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2018-03-21 12:24:47 +0100

    Första delen av labben

---
+++ Rockfestival/Rockfestival/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Rockfestival {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.bt_info = new System.Windows.Forms.Button();
+            this.lbx_info = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // bt_info
+            // 
+            this.bt_info.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.bt_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.bt_info.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bt_info.Location = new System.Drawing.Point(13, 13);
+            this.bt_info.Name = "bt_info";
+            this.bt_info.Size = new System.Drawing.Size(194, 104);
+            this.bt_info.TabIndex = 0;
+            this.bt_info.Text = "Info";
+            this.bt_info.UseVisualStyleBackColor = false;
+            this.bt_info.Click += new System.EventHandler(this.bt_info_Click);
+            // 
+            // lbx_info
+            // 
+            this.lbx_info.FormattingEnabled = true;
+            this.lbx_info.Location = new System.Drawing.Point(213, 12);
+            this.lbx_info.Name = "lbx_info";
+            this.lbx_info.Size = new System.Drawing.Size(456, 108);
+            this.lbx_info.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 596);
+            this.Controls.Add(this.lbx_info);
+            this.Controls.Add(this.bt_info);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_info;
+        private System.Windows.Forms.ListBox lbx_info;
+    }
+}
+
---
+++ Rockfestival/Rockfestival/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Rockfestival {
+    public partial class Form1 : Form {
+
+        NpgsqlConnection conn;
+        NpgsqlCommand cmd;
+        NpgsqlDataReader dr;
+
+        public Form1() {
+            InitializeComponent();
+            EstablishConnection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        void EstablishConnection() {            
+
+            string sqlCommand = "Server=pgserver.mah.se; Port=5432; User Id = ah9353; Password = khz0g68k; Database = mfrockfestival";
+
+                conn = new NpgsqlConnection(sqlCommand);
+                conn.Open();
+
+                cmd = new NpgsqlCommand("SELECT version(); ", conn);
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read()) {
+                    Console.WriteLine("connection established");
+                    Console.WriteLine("{0}", dr[0]);
+                    Console.WriteLine();
+                    //dr[int ordinal] dr[string name]
+                }
+                else {
+                    Console.WriteLine("Data does not exist");
+                }
+                dr.Close();
+            
+        }
+        void EnterCommand(string sqlCommand) {
+            cmd = new NpgsqlCommand(sqlCommand, conn);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine(sqlCommand);
+        }
+
+        private void bt_info_Click(object sender, EventArgs e) {
+            EnterCommand("Select * from spelschema;" );
+            while (dr.Read()) {
+                for (int i = 0; i < dr.VisibleFieldCount; i++) {
+                    lbx_info.Items.Add(dr[i]);
+                    Console.WriteLine(dr[i]);
+                }
+            }
+            dr.Close();
+
+        }
+    }
+
+}
+
